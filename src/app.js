9b36fd8fb9feef6de6f159abc915880b68e0a6e9
Zeksa <page.simon14@gmail.com>
2021-03-11 18:01:38 +0100

    Parcel 2

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+import { Pdf } from "./Pdf";
+
+const reactRoot = document.querySelector("#react-root");
+render(<Pdf />, reactRoot);
