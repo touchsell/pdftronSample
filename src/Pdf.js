4c930cf1920288c07935503f81177e7a6986ed95
AurelienS <aurelien.sberro@gmail.com>
2021-03-15 18:47:46 +0100

    babelrc with default config

--- src/Pdf.js
+++ src/Pdf.js
@@ -1,43 +1,43 @@
-import WebViewer from "@pdftron/webviewer";
-import React, { useEffect, useRef, useState } from "react";
+import WebViewer from '@pdftron/webviewer'
+import React, { useEffect, useRef, useState } from 'react'
 
 const PDFTRON_WEBVIEWER_OPTIONS = {
   licenseKey:
-    "The App Lab SAS(touch-sell.com):OEM:Touch & Sell::B+:AMS(20211107):" +
-    "77B5256304E7B60AB360B13AC982737860615F4CD7061A0395DD8E435CA55E37128A31F5C7",
-  path: "/wv",
+    'The App Lab SAS(touch-sell.com):OEM:Touch & Sell::B+:AMS(20211107):' +
+    '77B5256304E7B60AB360B13AC982737860615F4CD7061A0395DD8E435CA55E37128A31F5C7',
+  path: '/wv',
   fullAPI: true,
-  preloadWorker: "all",
-};
+  preloadWorker: 'all',
+}
 
 const smallPdf =
-  "https://usercontent.touch-sell.net/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBK0NMTXc9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--523a6a93e197bba42f4e3107f73ab33dd1cb7e1c/blob-eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBK0NMTXc9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--523a6a93e197bba42f4e3107f73ab33dd1cb7e1c.pdf";
+  'https://usercontent.touch-sell.net/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBK0NMTXc9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--523a6a93e197bba42f4e3107f73ab33dd1cb7e1c/blob-eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBK0NMTXc9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--523a6a93e197bba42f4e3107f73ab33dd1cb7e1c.pdf'
 
 const bigPdf =
-  "https://bo.touch-sell.net/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBNDdvTUE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--92b62f2c28069334bdc6ab1aedb439812b94a635/Pathfinder.optimized.pdf";
+  'https://bo.touch-sell.net/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBNDdvTUE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--92b62f2c28069334bdc6ab1aedb439812b94a635/Pathfinder.optimized.pdf'
 
-const mediumPdf = "/files/100page.pdf";
+const mediumPdf = '../../files/long2.pdf'
 
 export const Pdf = () => {
-  const viewer = useRef(null);
+  const viewer = useRef(null)
 
-  const [instance, setInstance] = useState();
+  const [instance, setInstance] = useState()
 
   useEffect(() => {
     WebViewer(PDFTRON_WEBVIEWER_OPTIONS, viewer.current).then((instance) => {
-      setInstance(instance);
-    });
-  }, []);
+      setInstance(instance)
+    })
+  }, [])
 
   return (
     <>
       <select
         onChange={(event) => {
-          instance.loadDocument(event.target.value);
-          instance.setLayoutMode(instance.LayoutMode.Single);
-          instance.setSwipeOrientation("horizontal");
-          instance.docViewer.setMargin(0);
-          instance.docViewer.setEnableAutomaticLinking(false);
+          instance.loadDocument(event.target.value)
+          instance.setLayoutMode(instance.LayoutMode.Single)
+          instance.setSwipeOrientation('horizontal')
+          instance.docViewer.setMargin(0)
+          instance.docViewer.setEnableAutomaticLinking(false)
         }}
       >
         <option value={smallPdf}>Small pdf</option>
@@ -45,10 +45,10 @@ export const Pdf = () => {
         <option value={bigPdf}>Big pdf</option>
       </select>
       <div
-        style={{ width: "100vw", height: "100vh" }}
-        data-cy="pdfViewer"
+        style={{ width: '100vw', height: '100vh' }}
+        data-cy='pdfViewer'
         ref={viewer}
       ></div>
     </>
-  );
-};
+  )
+}
